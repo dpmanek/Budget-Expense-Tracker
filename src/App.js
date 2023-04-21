26956143dd3b5819c358fcf47adfabbc3578b225
Deep Manek <dpmanek@gmail.com>
2023-04-21 19:51:11 -0400

    changing router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import Routesr from "./routes/routes";
+import './App.css';
+import {
+	// BrowserRouter
+	HashRouter,
+} from 'react-router-dom';
+import Routesr from './routes/routes';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routesr />
-    </BrowserRouter>
-  );
+	return (
+		<HashRouter basename="/">
+			<Routesr />
+		</HashRouter>
+	);
 }
 
 export default App;
