101f23addad0787b852e14e175c2e12184881f86
Deep Manek <dpmanek@gmail.com>
2023-04-21 19:44:43 -0400

    Updates

---
+++ precache-manifest.a28c7019da94fc7ca56699925d784990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c52e6a00038b7d04b25",
+    "url": "/Budget-Expense-Tracker/static/css/main.c84551b5.chunk.css"
+  },
+  {
+    "revision": "9c52e6a00038b7d04b25",
+    "url": "/Budget-Expense-Tracker/static/js/main.9c52e6a0.chunk.js"
+  },
+  {
+    "revision": "240f4daddc7ec9cd9fc7",
+    "url": "/Budget-Expense-Tracker/static/js/1.240f4dad.chunk.js"
+  },
+  {
+    "revision": "18ce455c478959f50f2d",
+    "url": "/Budget-Expense-Tracker/static/css/2.4f071a41.chunk.css"
+  },
+  {
+    "revision": "18ce455c478959f50f2d",
+    "url": "/Budget-Expense-Tracker/static/js/2.18ce455c.chunk.js"
+  },
+  {
+    "revision": "cab0cdc3015c37cae149",
+    "url": "/Budget-Expense-Tracker/static/js/runtime~main.cab0cdc3.js"
+  },
+  {
+    "revision": "48677f833ba2239c6ab19f09b7e78c4c",
+    "url": "/Budget-Expense-Tracker/index.html"
+  }
+];
