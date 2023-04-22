2e41bceeaa65756d9d1959bb28dd6675151d6be4
Deep Manek <dpmanek@gmail.com>
2023-04-21 20:25:28 -0400

    fix

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,39 +3,6 @@ import '../Footer/Footer.css';
 
 import { useNavigate } from 'react-router-dom';
 
-const Intro = () => {
-	return (
-		<div className="intro-div">
-			<div className="landing-body">
-				<div className="landing-text-info-area">
-					<p className="landing-text__title">Expense tracking</p>
-					<p className="landing-text__title">made easy</p>
-					<p className="landing-text__paragraph">
-						Stay on top of your spending with Expense Tracker. An online
-						tracking tool to help you better understand your habits and make
-						measurable change.{' '}
-					</p>
-					<button
-						className="btn btn-primary"
-						onClick={() => redirectRoute('/signup')}
-					>
-						Get Started
-					</button>
-				</div>
-				<div>
-					<img
-						className="pig"
-						src="./Budget-Expense-Tracker/pigIllustration.png"
-						alt=" "
-					/>
-				</div>
-			</div>
-		</div>
-	);
-};
-
-export default Intro;
-
 const Footer = () => {
 	let navigate = useNavigate();
 	const redirectRoute = (path) => {
